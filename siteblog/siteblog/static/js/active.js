b768909228855c257abb2462f551f284bb2290c7
SadFox16 <oleg.s.kerimov@yandex.com>
2022-07-02 00:24:44 +0300

    Single page view
    Single page view

--- siteblog/siteblog/static/js/active.js
+++ siteblog/siteblog/static/js/active.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $('.menu a').each(function(){
-        let location = window.location.protocol + '//' + window,location.host + window.location.pathname;
+        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
         let link = this.href;
         if(location == link){
             $(this).parent().addClass('active');

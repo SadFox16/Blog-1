b9fae3a1a06e2fd26b8a58f57e556f7e38033715
SadFox16 <oleg.s.kerimov@yandex.com>
2022-07-02 11:12:44 +0300

    Tags cloud

--- siteblog/blog/templatetags/sidebar.py
+++ siteblog/blog/templatetags/sidebar.py
@@ -9,3 +9,9 @@ register = template.Library()
 def get_popular(cnt=3):
     posts = Post.objects.order_by('-views')[:cnt]
     return {"posts": posts}
+
+
+@register.inclusion_tag('blog/tags_cloud_tpl.html')
+def get_tags_cloud():
+    tags = Tag.objects.all().order_by('title')
+    return {"tags": tags}
